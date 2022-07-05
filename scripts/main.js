04371fc6552d4848bf665211bae216bee5c4c475
Joseph <frasier.joseph@gmail.com>
2022-07-05 14:57:19 -0500

    weather code

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
-window.addEventListener('DOMContentLoaded', function() {
+const dealerHand = document.getElementById("dealer-hand");
+const playerHand = document.getElementById("player-hand");
+const deck = [];
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+const makeDeck = (rank, suit) => {
+  const card = {
+    rank: rank,
+    suit: suit,
+    pointValue: rank > 10 ? 10 : rank,
+  };
+  deck.push(card);
+};
+
+for (let suit of suits) {
+  for (const rank of ranks) {
+    makeDeck(rank, suit);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
   // Execute after page load
-})
+});
